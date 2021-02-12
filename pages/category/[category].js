e125a765cdb24c50cf4069f18853fe2724af6da7
Jeremie Litzler <jeremie.litzler@conduent.com>
2021-02-12 14:17:52 +0100

    #3-fix: comment code in [category].js

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -1,37 +1,37 @@
-import Layout from '@components/Layout';
-import PostList from '@components/PostList';
+// import Layout from '@components/Layout';
+// import PostList from '@components/PostList';
 
-import getPosts from 'functions/getPosts';
+// import getPosts from 'functions/getPosts';
 
-const Index = ({ posts, title, description, ...props }) => {
-  return (
-    <>
-      <Layout pageTitle={title} description={description}>
-        <h1 className='title'>My articles!</h1>
-        {/* The parameter is the attribute value to use where the component is used.*/}
-        <PostList posts={posts} />
-      </Layout>
-    </>
-  );
-};
+// const Index = ({ posts, title, description, ...props }) => {
+//   return (
+//     <>
+//       <Layout pageTitle={title} description={description}>
+//         <h1 className='title'>My articles!</h1>
+//         {/* The parameter is the attribute value to use where the component is used.*/}
+//         <PostList posts={posts} />
+//       </Layout>
+//     </>
+//   );
+// };
 
-export default Index;
+// export default Index;
 
-export async function getStaticProps() {
-  const configData = await import(`../siteconfig.json`);
+// export async function getStaticProps() {
+//   const configData = await import(`../siteconfig.json`);
 
-  const posts = ((context) => {
-    return getPosts(context, false);
-  })(require.context('../posts', true, /\.md$/));
-  //console.log(posts);
-  const filteredPosts = posts.filter(
-    (post) => post.frontmatter.category === 'Category 1',
-  );
-  return {
-    props: {
-      filteredPosts,
-      title: configData.default.title,
-      description: configData.default.description,
-    },
-  };
-}
+//   const posts = ((context) => {
+//     return getPosts(context, false);
+//   })(require.context('../posts', true, /\.md$/));
+//   //console.log(posts);
+//   const filteredPosts = posts.filter(
+//     (post) => post.frontmatter.category === 'Category 1',
+//   );
+//   return {
+//     props: {
+//       filteredPosts,
+//       title: configData.default.title,
+//       description: configData.default.description,
+//     },
+//   };
+// }

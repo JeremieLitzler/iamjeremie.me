63ba7805507fff842e7b14ea0cf85b0adeb07b56
Jeremie Litzler <jeremie.litzler@conduent.com>
2021-02-15 18:09:11 +0100

    #24-feat: update about

--- pages/about.js
+++ pages/about.js
@@ -3,15 +3,23 @@ import Layout from '@components/Layout';
 const About = ({ title, description, ...props }) => {
   return (
     <>
-      <Layout pageTitle={`${title} | Who`} description={description}>
-        <h1 className='title'>Hello, I am Jeremie.</h1>
-        <p className='description'>
-          In 2009, I started my career. <br /> From Trust5 in Ireland to
-          Conduent in France, I am engaged to improve the daily life of
-          businesses, individuals and more through web-based applications and
-          services.
-          <br /> I have acquired a wide range of skills in software development
-          for the Web and I thrive to learn new tricks every day.
+      <Layout pageTitle={`${title} | The Who`} description={description}>
+        <h1 className='title title-about'>Hello, I am Jeremie.</h1>
+        <img
+          className='profile-pic'
+          src='static/images/profilepic-400w.jpg'
+          alt="Jeremie climbing the Kandersteg's viaferrata"
+        />
+        <h2>Career</h2>
+        <p>In 2009, I started my career.</p>
+        <p>
+          From Trust5 in Ireland to Conduent Business Solutions in France, I am
+          passionate about improving the daily life of businesses, individuals
+          and more through web-based applications and services.
+        </p>
+        <p>
+          I have acquired a wide range of skills in software development for the
+          Web and I thrive to learn new tricks every day.
         </p>
         <p>
           For more details about my work experience and skill, come read{' '}
@@ -22,19 +30,21 @@ const About = ({ title, description, ...props }) => {
           >
             my resume here
           </a>
-          . Otherwise, explore my articles!
+          .
         </p>
-        {/*<p>
-          You can check out the{' '}
-          <a href='https://github.com/cassidoo/next-netlify-blog-starter'>
-            repo here.
-          </a>{' '}
-          If you'd like to build it yourself,{' '}
-          <a href='https://url.netlify.com/ByVW0bCF8'>
-            here is a tutorial on how to do so
-          </a>
-          !
-        </p>*/}
+        <h2>Hobbies</h2>
+        <p>
+          Otherwise, explore my articles. I am sharing more than software
+          related thoughts and experiences. It includes and is not limited to:
+        </p>
+        <ul>
+          <li>Health and diet</li>
+          <li>Living more sustainably in our environnement</li>
+          <li>Gardening</li>
+          <li>Product reviews that I have purchased once or more</li>
+        </ul>
+        <h2>What else?</h2>
+        <p></p>
       </Layout>
     </>
   );

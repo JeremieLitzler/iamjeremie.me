ebc7bab826ccf4d6b335e65f094529633ed78412
Jeremie Litzler <jeremie.litzler@conduent.com>
2021-02-19 07:21:34 +0100

    #28-fix: attempt to fix the golbal css load

---
+++ components/Footer.js
@@ -0,0 +1,106 @@
+import Link from 'next/link';
+
+const labels = {
+  facebookLinkText: 'Visit my Facebook page',
+  twitterLinkText: 'Follow me on Twitter',
+  linkedInLinkText: 'Review my LinkedIn profile',
+  mediumLinkText: 'Support me on Medium',
+};
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <section>
+          <div className='social-links'>
+            <a
+              className='social-link'
+              href='https://facebook.com/MadeByJeremie'
+              rel='noopener noreferrer'
+              target='_blank'
+              title={labels.facebookLinkText}
+              aria-label={labels.facebookLinkText}
+            >
+              {labels.facebookLinkText}
+            </a>
+            <a
+              className='social-link'
+              href='https://twitter.com/puzzlout'
+              rel='noopener noreferrer'
+              title={labels.twitterLinkText}
+              aria-label={labels.twitterLinkText}
+            >
+              {labels.twitterLinkText}
+            </a>
+            <a
+              className='social-link'
+              href='https://jeremie-litzler.medium.com/'
+              rel='noopener noreferrer'
+              title={labels.mediumLinkText}
+              aria-label={labels.mediumLinkText}
+            >
+              {labels.mediumLinkText}
+            </a>
+            <a
+              className='social-link'
+              href='https://www.linkedin.com/in/jeremielitzler'
+              rel='noopener noreferrer'
+              title={labels.linkedInLinkText}
+              aria-label={labels.linkedInLinkText}
+            >
+              {labels.linkedInLinkText}
+            </a>
+          </div>
+          <p className='fine-print'>
+            This site is built {''}
+            <a
+              href='https://url.netlify.com/ByVW0bCF8'
+              target='_blank'
+              rel='noreferrer'
+            >
+              from my GitHub repository
+            </a>
+            {''} with Next, and is easily{' '}
+            <a
+              href='https://url.netlify.com/r1j6ybSYU'
+              target='_blank'
+              rel='noreferrer'
+            >
+              deployable on Netlify
+            </a>
+            .
+          </p>
+          <p className='fine-print'>
+            You can look at{' '}
+            <a
+              href='https://github.com/cassidoo/next-netlify-blog-starter'
+              title="Link to Cassidy's repository"
+            >
+              the original project of Cassidy Williams on GitHub
+            </a>
+            , and {''}
+            <a
+              href='https://url.netlify.com/ByVW0bCF8'
+              target='_blank'
+              rel='noreferrer'
+            >
+              her Netlify blog post
+            </a>{' '}
+            on how to build it.
+          </p>
+          <p className='fine-print'>
+            Built with and served by{' '}
+            <a
+              className='netlify-heart'
+              href='http://netlify.com'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <img src='/netliheart.svg' alt='A Netlify Heart' />
+            </a>
+          </p>
+        </section>
+      </footer>
+    </>
+  );
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,6 @@
 import { Helmet } from 'react-helmet';
+import Footer from './Footer';
 import Header from './Header';
-
-const labels = {
-  facebookLinkText: 'Visit my Facebook page',
-  twitterLinkText: 'Follow me on Twitter',
-  linkedInLinkText: 'Review my LinkedIn profile',
-  mediumLinkText: 'Support me on Medium',
-};
 
 export default function Layout({ children, pageTitle, description, ...props }) {
   return (
@@ -21,104 +15,14 @@ export default function Layout({ children, pageTitle, description, ...props }) {
           href='https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700&family=Open+Sans:wght@400;700&family=Sacramento&display=swap'
           rel='stylesheet'
         />
-        <link rel='stylesheet' href='/static/style.css' />
+        {/* <link rel='stylesheet' href='/static/style.css' /> */}
       </Helmet>
       <section className='layout'>
         <Header />
         <main role='main' className='content'>
           {children}
         </main>
-        <footer>
-          <section>
-            <div className='social-links'>
-              <a
-                className='social-link'
-                href='https://facebook.com/MadeByJeremie'
-                rel='noopener noreferrer'
-                target='_blank'
-                title={labels.facebookLinkText}
-                aria-label={labels.facebookLinkText}
-              >
-                {labels.facebookLinkText}
-              </a>
-              <a
-                className='social-link'
-                href='https://twitter.com/puzzlout'
-                rel='noopener noreferrer'
-                title={labels.twitterLinkText}
-                aria-label={labels.twitterLinkText}
-              >
-                {labels.twitterLinkText}
-              </a>
-              <a
-                className='social-link'
-                href='https://jeremie-litzler.medium.com/'
-                rel='noopener noreferrer'
-                title={labels.mediumLinkText}
-                aria-label={labels.mediumLinkText}
-              >
-                {labels.mediumLinkText}
-              </a>
-              <a
-                className='social-link'
-                href='https://www.linkedin.com/in/jeremielitzler'
-                rel='noopener noreferrer'
-                title={labels.linkedInLinkText}
-                aria-label={labels.linkedInLinkText}
-              >
-                {labels.linkedInLinkText}
-              </a>
-            </div>
-            <p className='fine-print'>
-              This site is built {''}
-              <a
-                href='https://url.netlify.com/ByVW0bCF8'
-                target='_blank'
-                rel='noreferrer'
-              >
-                from my GitHub repository
-              </a>
-              {''} with Next, and is easily{' '}
-              <a
-                href='https://url.netlify.com/r1j6ybSYU'
-                target='_blank'
-                rel='noreferrer'
-              >
-                deployable on Netlify
-              </a>
-              .
-            </p>
-            <p className='fine-print'>
-              You can look at{' '}
-              <a
-                href='https://github.com/cassidoo/next-netlify-blog-starter'
-                title="Link to Cassidy's repository"
-              >
-                the original project of Cassidy Williams on GitHub
-              </a>
-              , and {''}
-              <a
-                href='https://url.netlify.com/ByVW0bCF8'
-                target='_blank'
-                rel='noreferrer'
-              >
-                her Netlify blog post
-              </a>{' '}
-              on how to build it.
-            </p>
-            <p className='fine-print'>
-              Built with and served by{' '}
-              <a
-                className='netlify-heart'
-                href='http://netlify.com'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <img src='/netliheart.svg' alt='A Netlify Heart' />
-              </a>
-            </p>
-          </section>
-        </footer>
+        <Footer />
       </section>
       {/* <script src='/static/menu-scroll.js'></script> */}
     </>
